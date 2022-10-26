4bcd09ce9a989a22eb5d30dbc9bb43976146599f
dim.tutushkin <egiktumane916@gmail.com>
2022-10-26 21:28:04 +0300

    Сделан блок testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ modalCloses.forEach((modalClose) => {
   })
 })
 
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
   cssMode: true,
   loop: true,
 
@@ -91,4 +91,22 @@ let swiper = new Swiper('.portfolio__container', {
     el: ".swiper-pagination",
     clickable: true,
   },
+});
+
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    }
+  }
 });
